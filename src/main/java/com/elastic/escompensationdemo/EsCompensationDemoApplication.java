eaf7095d7af7f7e96d19a50d2e22874f087fff4b
divyakumaravel <divyakk98@gmail.com>
2024-03-05 23:21:24 +0530

    Divya | Initial Commit

---
+++ src/main/java/com/elastic/escompensationdemo/EsCompensationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.elastic.escompensationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsCompensationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsCompensationDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elastic/escompensationdemo/EsCompensationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elastic.escompensationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsCompensationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
